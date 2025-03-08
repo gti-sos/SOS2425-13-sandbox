bcf27f949366831b1dd4bd27a20c59db6616d11a
Darío López <darlopvil@alum.us.es>
2025-03-08 09:55:48 +0100

    Actualizado con clase L05

--- index.js
+++ index.js
@@ -1,35 +1,40 @@
-console.log("Init script");
 
-//Framework que instancias a traves de la funcion
 const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 16078;
-const cool = require('cool-ascii-faces');
-
-//Configurando el server para las peticiones
-/*request me puede servir para mandar un formulario y mandarlo
-*/
+const BASE_API = "/api/v1";
 
 app.use("/", express.static("./public"));
+app.use(express.json()); //Para que pueda interpretar el body de las peticiones POST
 
-app.get("/hello", (request, response) => {
-    response.send("Hola desde el servidor!");
+let contacts = [
+    { name: "Peter",
+        phone: "123456789"
+    },
+    { name: "Mary",
+        phone: "987654321"
+    }
+];
+
+
+app.get(BASE_API + "/contacts", (request, response) => {
+    console.log("New GET to /contacts");
+    
+    response.send(JSON.stringify(contacts,null,2));
 });
 
-app.get("/cool", (request, response) => {
-    response.send(cool());
+app.post(BASE_API + "/contacts", (request, response) => {
+    console.log("POST to /contacts");
+    let newContact = request.body;
+    contacts.push(newContact);
+    response.sendStatus(201);
 });
 
-//Lanzar el server por un puerto
-/*
-Es necesario poner el mensaje de "OK escuchando" dentro de la función listen como callback.
-El server tarda un tiempo hasta que se pone a escuchar por el puerto deseado.
-Pero el "console.log()" es asíncrono, por lo que se ejecuta antes de que el server esté listo.
-Con el uso del callback, sacamos el mensaje de "OK escuchando" cuando está configurado completamente el server.
-*/
+
+
+
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
 });
 
-//console.log(`Finish setup`); //Este mensaje se ejecuta antes de que el server esté listo
 
