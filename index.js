efdbb448797232a1ef5cc3c067435a3b89fd6bb0
Dario Lopez <darlopvil@alum.us.es>
2025-04-12 16:22:48 +0200

    L07 updated

--- index.js
+++ index.js
@@ -1,19 +1,19 @@
 //const express = require("express");
 import express from "express";
 import  { loadBackend } from "./src/back/index.js";
-import {handler} from "./src/front/build/handler.js"
+import {handler} from "./src/front/build/handler.js";
 
 const app = express();
 const PORT = process.env.PORT || 16078;
 
 app.use(express.json());
 
-loadBackend(app) //Cargamos primero el backend para que esté disponible para el frontend
+loadBackend(app); //Cargamos primero el backend para que esté disponible para el frontend
 
 app.use(handler); // Express usará este handler para servir la aplicación Svelte 
 
 
 
-app.listen(PORT,()=>{ 
+app.listen(PORT, () => { 
     console.log(`Server running on port ${PORT}!`);
 });
